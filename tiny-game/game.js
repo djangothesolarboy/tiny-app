1d98cac0ec5d9154229295227168641d3bce1cc7
Damien Darko <warwickjunglex@gmail.com>
2021-09-28 18:14:37 -0400

    Game working

--- tiny-game/game.js
+++ tiny-game/game.js
@@ -4,7 +4,7 @@ gs = ts = 20;
 ax = ay = 15;
 xv = yv = 0;
 
-body = [];
+body = []; // { x: px, y: py }
 segments = 5;
 
 // game logic
@@ -15,24 +15,58 @@ const game = () => {
     context.fillStyle = '#000000';
     context.fillRect(0, 0, canvas.width, canvas.height);
     context.fillStyle = '#8fdfff';
+    
+    if (px < 0) px = ts - 1;
+    if (px > ts) px = 0;
+    if (py < 0) py = ts - 1;
+    if (py > ts - 1) py = 0;
+
+    for (let i = 0; i < body.length; i++) {
+        context.fillRect(body[i].x * gs, body[i].y * gs, gs - 2, gs - 2);
+        if (body[i].x === px && body[i].y === py) segments = 5;
+    }
+
+    body.push({ x: px, y: py });
+
+    while (body.length > segments) body.shift();
+
+    // eating
+    if (ax === px && ay === py) {
+        segments++;
+        ax = Math.floor(Math.random() * ts);
+        ay = Math.floor(Math.random() * ts);
+    }
+
+    context.fillStyle = '#8fdf2f';
+    context.fillRect(ax * gs, ay * gs, gs - 2, gs - 2);
+
+    document.getElementById('score').innerHTML = `Score: ${segments}`;
+    // document.getElementById('high-score').innerHTML = `Highscore: ${segments}`;
+
+    // console.log(segments)
 }
 
 
 // controls
 const keyDown = e => {
-    console.log(e.keyCode);
+    // let left = (65, 37);
+    // console.log(e.keyCode);
     switch(e.keyCode) {
         case(65): //37
-            console.log('left');
+            xv = -1;
+            yv = 0;
             break;
         case(87): // 38
-            console.log('up');
+            xv = 0;
+            yv = -1;
             break;
         case(68): // 39
-            console.log('right');
+            xv = 1;
+            yv = 0;
             break;
         case(83): // 40
-            console.log('down');
+            xv = 0;
+            yv = 1;
             break;
     }
 }
@@ -42,8 +76,10 @@ const init = () => {
     context = canvas.getContext('2d');
     document.addEventListener('keydown', keyDown);
     setInterval(game, 1000/15);
-    // alert('working');
 }
+
+// change game interval to chance speed/difficulty
+// count segments for score
 
 // window.onload = init();
 
